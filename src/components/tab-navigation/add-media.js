9253509d0bf75f3e37ab664314804da3b6ec5177
Hien Phung <pkhien95@gmail.com>
2020-03-31 15:08:06 +0700

    Link to Spotify play list

--- src/components/tab-navigation/add-media.js
+++ src/components/tab-navigation/add-media.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Linking,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 function AddMediaTab() {
+  const [url, setUrl] = useState(
+    'https://open.spotify.com/track/1U8bwOTAtnqlWMCn1mwYyj?si=d7uCaWh6TfCWkiRH3oioJA',
+  );
+  
+  const onOpenPress = () => {
+    Linking.canOpenURL(url).then(supported => {
+      if (supported) {
+        Linking.openURL(url);
+      }
+    });
+  };
+  
+  const onChangeUrl = (text: string) => {
+    setUrl(text);
+  };
+  
   return (
     <View style={styles.container}>
-      <Text>AddMediaTab</Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder={'Enter playlist url'}
+        onChangeText={onChangeUrl}
+        value={url}
+      />
+      <TouchableOpacity style={styles.openButton} onPress={onOpenPress}>
+        <Text style={styles.openButtonText}>Open In Spotify</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -16,5 +47,28 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textInput: {
+    height: 60,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#2196f3',
+    marginHorizontal: 15,
+    padding: 10,
+    fontSize: 16,
+    alignSelf: 'stretch',
+  },
+  openButton: {
+    marginHorizontal: 30,
+    backgroundColor: '#2196f3',
+    padding: 15,
+    marginTop: 20,
+    borderRadius: 5,
+  },
+  openButtonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
