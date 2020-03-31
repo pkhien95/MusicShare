a69a27d1684682b673b69228387c3cf5dd056556
Hien Phung <pkhien95@gmail.com>
2020-03-31 15:28:27 +0700

    Open album from image

--- src/components/tab-navigation/add-media.js
+++ src/components/tab-navigation/add-media.js
@@ -1,40 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
+  Image,
   Linking,
   StyleSheet,
-  Text,
-  TextInput,
   TouchableOpacity,
   View,
 } from 'react-native';
 
 function AddMediaTab() {
-  const [url, setUrl] = useState(
-    'https://open.spotify.com/track/1U8bwOTAtnqlWMCn1mwYyj?si=d7uCaWh6TfCWkiRH3oioJA',
-  );
-  
+  const imageUrl =
+    'https://i.scdn.co/image/ab67616d0000b2738233b7f811afd19bbbcd3743';
+
+  const albumUrl = 'https://open.spotify.com/album/5idokHIibbDsmLLHyvwwQp';
+
   const onOpenPress = () => {
-    Linking.canOpenURL(url).then(supported => {
+    Linking.canOpenURL(albumUrl).then(supported => {
       if (supported) {
-        Linking.openURL(url);
+        Linking.openURL(albumUrl);
       }
     });
   };
-  
-  const onChangeUrl = (text: string) => {
-    setUrl(text);
-  };
-  
+
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.textInput}
-        placeholder={'Enter playlist url'}
-        onChangeText={onChangeUrl}
-        value={url}
-      />
-      <TouchableOpacity style={styles.openButton} onPress={onOpenPress}>
-        <Text style={styles.openButtonText}>Open In Spotify</Text>
+      <TouchableOpacity style={styles.imageContainer} onPress={onOpenPress}>
+        <Image
+          source={{
+            uri: imageUrl,
+          }}
+          style={styles.image}
+        />
       </TouchableOpacity>
     </View>
   );
@@ -48,27 +43,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  textInput: {
-    height: 60,
-    backgroundColor: 'white',
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: '#2196f3',
-    marginHorizontal: 15,
-    padding: 10,
-    fontSize: 16,
-    alignSelf: 'stretch',
-  },
-  openButton: {
-    marginHorizontal: 30,
-    backgroundColor: '#2196f3',
-    padding: 15,
-    marginTop: 20,
-    borderRadius: 5,
-  },
-  openButtonText: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: 'bold',
+  imageContainer: {},
+  image: {
+    width: 200,
+    height: 200,
   },
 });
